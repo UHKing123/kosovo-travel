1b07ed2d199660f08d8d70c9258db378461d63ae
Valton <valtonm75@gmail.com>
2021-06-23 11:05:29 +0200

    Started styling header

---
+++ src/components/header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Header.css'
+import logo from '../../Images/logo.png'
+import { BsSearch } from "react-icons/bs";
+
+export default function header() {
+    return (
+        <>
+            <header>
+                <div className="container">
+                     <div className="header-wrapper">
+                        <a href="#"><img src="#" />LOGO</a>
+                        <div className="header_navigation">
+                            <ul>
+                                <li>
+                                    <a href="#">Tickets</a>
+                                </li>
+                                <li>
+                                    <a href="#">Places to go</a>
+                                </li>
+                                <li>
+                                    <a href="#">Things to do</a>
+                                </li>
+                                <li>
+                                    <a href="#">Plan your trip</a>
+                                </li>
+                            </ul>
+                            <BsSearch />
+                        </div>
+                     </div>
+                </div>
+            </header>           
+        </>
+    )
+}
